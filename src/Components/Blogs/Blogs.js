6654283bba3e8d8f47c567ce87a22378548489ec
minhazpro3 <minhazfr8@gmail.com>
2022-01-05 00:47:59 +0600

    text-change

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -10,7 +10,7 @@ const Blogs = () => {
         <div className="bg-gray-800 pb-8 "  id="blogs">
           
             <h2 data-aos="fade-up"    className=" text-white mt-5 justify-center text-2xl bg-green-500   px-8 d-inline-block w-32 mx-auto py-3 px-5 mb-8 rounded-full rounded-br-none rounded-tl-none">Blogs</h2>
-            <h2 className="text-3xl text-white  text-center py-32">Blogs is not yet</h2>
+            <h2 className="text-3xl text-white  text-center py-32">Blogs waiting is not yet</h2>
         </div>
     );
 };
