4db7408b17825eea0d2bd4ea62247f819b7bb253
minhazpro3 <minhazpro1@gmail.com>
2022-01-20 20:52:24 +0600

    tap change

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,39 +3,39 @@ import React, { useEffect } from 'react';
 import 'aos/dist/aos.css';
 
 const About = () => {
-  useEffect(()=>{
+  useEffect(() => {
     AOS.init();
-   })
-    return (
-      <div className=" " id="about">     
-           <div className="container mx-auto">
-           <h2  data-aos="fade-up"    className=" mt-5 justify-center text-2xl bg-green-500   px-8 d-inline-block w-44 text-white mx-auto py-3 px-5 mb-8 rounded-full rounded-br-none rounded-tl-none">About me</h2>
-                
-                <div className="grid my-8 sm:grid-cols-1 md:grid-cols-2 gap-4 flex  items-center">
-                <div data-aos="fade-right"  className="px-5">
-              <img className="w-full" src="https://i.ibb.co/vmcQDGz/minhazpic-removebg-preview.png" alt="" />
-              </div>
-              <div data-aos="fade-right" className=" text-white align-center px-5">
-                <h3 className="">Hello, I’m  Minhaz, web-developer based on Paris. I have rich experience in web site design & building and customization. Also I am good at</h3>
-               <div className="flex flex-wrap gap-3 mt-5 ">
-               <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block  pe-0 border-4 border-blue-500 border-opacity-75">React.js</h5>
-               <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block  pe-0 border-4 border-blue-500 border-opacity-75">JavaScript</h5>
-               <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Bootstrap</h5>
-               <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">TailwindCss</h5>
-               <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Meterial-UI</h5>
-               <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Css3</h5>
-               <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Node.js</h5>
-               <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Mongodb</h5>
-               <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Express.js</h5>
-               <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Firebase Authentication</h5>
-               </div>
-              </div>
-              
-            </div>
-        </div>
-        </div>
+  })
+  return (
+    <div className=" " id="about">
+      <div className="container mx-auto">
+        <h2 data-aos="fade-up" className=" mt-5 justify-center text-2xl bg-green-500   px-8 d-inline-block w-44 text-white mx-auto py-3 px-5 mb-8 rounded-md ">About me</h2>
 
-    );
+        <div className="grid my-8 sm:grid-cols-1 md:grid-cols-2 gap-4 flex  items-center">
+          <div data-aos="fade-right" className="px-5">
+            <img className="w-full" src="https://i.ibb.co/wWWn3DP/minhazpic-removebg-preview-removebg-preview.png" alt="" />
+          </div>
+          <div data-aos="fade-right" className=" text-white align-center px-5">
+            <h3 className="">Hello, I’m  Minhaz, web-developer based on Paris. I have rich experience in web site design & building and customization. Also I am good at</h3>
+            <div className="flex flex-wrap gap-3 mt-5 ">
+              <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block  pe-0 border-4 border-blue-500 border-opacity-75">React.js</h5>
+              <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block  pe-0 border-4 border-blue-500 border-opacity-75">JavaScript</h5>
+              <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Bootstrap</h5>
+              <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">TailwindCss</h5>
+              <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Meterial-UI</h5>
+              <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Css3</h5>
+              <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Node.js</h5>
+              <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Mongodb</h5>
+              <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Express.js</h5>
+              <h5 className=" text- 1xl   rounded-full rounded-bl-none px-3 d-inline-block   pe-0 border-4 border-blue-500 border-opacity-75">Firebase Authentication</h5>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  );
 };
 
 export default About;
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,55 +6,55 @@ import ScrollToTop from "react-scroll-to-top";
 
 
 const Contact = () => {
-  useEffect(()=>{
+  useEffect(() => {
     AOS.init();
-   })
-    
-  
+  })
+
+
   function onSubmit(e) {
     e.preventDefault();
 
     emailjs.sendForm('gmailMessage', 'template_n7s66zn', e.target, 'user_JYf3GchBoPDlzJ5tKJYqC')
       .then((result) => {
-          alert('message send successful !')
+        alert('message send successful !')
       }, (error) => {
-          
+
       });
-      e.target.reset()
+    e.target.reset()
   };
-    return (
-      <div  className="bg-blue-200 " id="contact">
-       
-      
-        <div className="container mx-auto">
-        <ScrollToTop smooth top='20' style={{paddingLeft: '5px'  }} />
+  return (
+    <div className="bg-blue-200 " id="contact">
+
+
+      <div className="container mx-auto">
+        <ScrollToTop smooth top='20' style={{ paddingLeft: '5px' }} />
         <div className="pt-5">
-        <h2  data-aos="fade-up" className=" text-white text-2xl bg-green-500   px-3 d-inline-block w-56 mx-auto py-3 pl-6  rounded-full rounded-br-none rounded-tl-none">Contact With Me</h2>
+          <h2 data-aos="fade-up" className=" text-white text-2xl bg-green-500   px-3 d-inline-block w-56 mx-auto py-3 pl-6  rounded-md">Contact With Me</h2>
         </div>
 
-            <div className="grid  sm:grid-cols-1 md:grid-cols-2 gap-2 mt-8">
-           
-            <div data-aos="fade-right">
-              <img  src="https://i.ibb.co/C0hKvw1/rvb-de-base-185361734-removebg-preview.png" alt="" />
-              </div>
-              <div data-aos="fade-right"     className="  border-2 p-8">
-              
-              <form onSubmit={onSubmit}>
-      <input className=" border-4 w-full px-1 " name="name" required type="text"  placeholder="Name" />
-      <br/>
-      <input className="my-3 border-4 w-full px-1" name="email" required type="email" placeholder="Email" />
-      <br/>
-      <textarea className="mb-3 border-4 w-full px-1 " style={{height: "160px"}} type="text"  name="message" placeholder="Message"  required />
-    <br/>
-      <input className="w-full bg-green-400 text-white  py-1 rounded cursor-allowed"  type="submit" value="Send message" />
-    </form>
-   
-              </div>
-             
-            </div>
+        <div className="grid  sm:grid-cols-1 md:grid-cols-2 gap-2 mt-8">
+
+          <div data-aos="fade-right">
+            <img src="https://i.ibb.co/C0hKvw1/rvb-de-base-185361734-removebg-preview.png" alt="" />
+          </div>
+          <div data-aos="fade-right" className="  border-2 p-8">
+
+            <form onSubmit={onSubmit}>
+              <input className=" border-4 w-full px-1 " name="name" required type="text" placeholder="Name" />
+              <br />
+              <input className="my-3 border-4 w-full px-1" name="email" required type="email" placeholder="Email" />
+              <br />
+              <textarea className="mb-3 border-4 w-full px-1 " style={{ height: "160px" }} type="text" name="message" placeholder="Message" required />
+              <br />
+              <input className="w-full bg-green-400 text-white  py-1 rounded cursor-allowed" type="submit" value="Send message" />
+            </form>
+
+          </div>
+
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
